f1e618c0a029ac2e763294af6f406a7c618e9f3a
Renato Felix <renatofelixss@hotmail.com>
2018-03-14 20:27:19 -0300

    ClienteDao implementação

--- src/main/java/br/ufpi/carrinho/controller/ClienteBean.java
+++ src/main/java/br/ufpi/carrinho/controller/ClienteBean.java
@@ -6,10 +6,13 @@ package br.ufpi.carrinho.controller;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.ufpi.carrinho.dao.ClienteDao;
 import br.ufpi.carrinho.model.Cliente;
 
 /**
@@ -22,8 +25,8 @@ public class ClienteBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-//	 @Inject
-//	 private ClienteDao clienteDao;
+	@Inject
+	private ClienteDao clienteDao;
 
 	private Cliente cliente;
 
@@ -38,9 +41,51 @@ public class ClienteBean implements Serializable {
 	public void init() {
 		this.cliente = new Cliente();
 	}
-	
+
 	public void salvar() {
+		String cpf = this.cpf.replaceAll("\\.", "").replaceAll("\\-", "");
 		
+		if (clienteDao.buscar(cpf) == null) {
+			cliente.setCpf(cpf);
+			clienteDao.salvar(cliente);
+			cliente = new Cliente();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sucesso!", "Cliente salvo com sucesso!"));
+		}else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro!", "Já existe um cliente com o cpf " + cpf));
+		}
+
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
 	}
 
 }
---
+++ src/main/java/br/ufpi/carrinho/dao/ClienteDao.java
@@ -0,0 +1,87 @@
+package br.ufpi.carrinho.dao;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import br.ufpi.carrinho.model.Cliente;
+import br.ufpi.carrinho.model.Produto;
+
+@Stateless
+public class ClienteDao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public ClienteDao() {
+	}
+
+	public Cliente procurarClientePorEmailSenha(String email, String senha) {
+		TypedQuery<Cliente> query = em
+				.createQuery("SELECT c FROM Cliente c WHERE c.email = :email AND c.senha = :senha", Cliente.class);
+		query.setParameter("email", email);
+		query.setParameter("senha", convertStringToMd5(senha));
+
+		List<Cliente> clientes = query.getResultList();
+
+		if (clientes.size() == 1)
+			return clientes.get(0);
+
+		return null;
+	}
+
+	public String convertStringToMd5(String valor) {
+		MessageDigest mDigest;
+		try {
+
+			// Instanciamos o nosso HASH MD5, poderíamos usar outro como
+			// SHA, por exemplo, mas optamos por MD5.
+			mDigest = MessageDigest.getInstance("MD5");
+
+			// Convert a String valor para um array de bytes em MD5
+			byte[] valorMD5 = mDigest.digest(valor.getBytes("UTF-8"));
+
+			// Convertemos os bytes para hexadecimal, assim podemos salvar
+			// no banco para posterior comparação se senhas
+			StringBuffer sb = new StringBuffer();
+			for (byte b : valorMD5) {
+				sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1, 3));
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<Cliente> listar() {
+		TypedQuery<Cliente> query = em.createQuery("Select c from Cliente c", Cliente.class);
+		return query.getResultList();
+	}
+
+	public void salvar(Cliente cliente) {
+		String senha = convertStringToMd5(cliente.getSenha());
+		cliente.setSenha(senha);
+		em.merge(cliente);
+	}
+
+	public Cliente buscar(String cpf) {
+		TypedQuery<Cliente> query = 
+				em.createQuery("Select c from Cliente c WHERE c.cpf like :cpf", Cliente.class)
+				.setParameter("cpf", cpf);
+		return query.getSingleResult();
+	}
+
+}
